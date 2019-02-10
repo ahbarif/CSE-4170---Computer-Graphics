005ddcbebc217f94e3adef7af434d87442ed2fdf
ahbarif <ahb.arif@gmail.com>
2019-02-11 00:19:25 +0600

    Update Ellips cc.cpp

--- Ellips cc.cpp
+++ Ellips cc.cpp
@@ -5,11 +5,11 @@ void drawElipse(int a,int b)
     drawPixel(x,y);
     while(b*b*(4*x-2) > 4*a*a*(y+1))
     {
-        if(d<0){//delN
+        if(d<0){
             d += 4*a*a*(2*y+3);
             y++;
         }
-        else{//delNW
+        else{ //nw
             d += 4*b*b*(-2*x+2) + 4*a*a*(2*y+3);
             y++;
             x--;
@@ -20,12 +20,12 @@ void drawElipse(int a,int b)
     while(x>0)
     {
         
-        if(d<0){//delNW
+        if(d<0){ //nw
             d += 4*b*b*(-2*x+3) + 4*a*a*(2*y+2);
             y++;
             x--;
         }
-        else{//delW
+        else{
             d += 4*b*b*(-2*x+3);
             x--;
         }
